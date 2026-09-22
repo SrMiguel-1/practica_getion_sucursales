df454180b70a89467fb44041830996ba9396dc94
nikol <sarithortiz26@gmail.com>
2026-09-21 19:39:37 -0500

    Agrega listado de sucursales

---
+++ listas/registros.py
@@ -0,0 +1,15 @@
+import json
+
+def listar_sucursales():
+    with open("sucursales.json", "r", encoding="utf-8") as archivo:
+        sucursales = json.load(archivo)
+
+    print("------ LISTA DE SUCURSALES -------------")
+
+    for sucursal in sucursales:
+        print("--------------------------")
+        print("Nombre:", sucursal["nombre"])
+        print("Dirección:", sucursal["direccion"])
+        print("Teléfono:", sucursal["telefono"])
+        print("Gerente:", sucursal["gerente"])
+        print("Estado:", sucursal["estado"])
